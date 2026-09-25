f3184a93e13bc0a02c1e9e35f37951d8ba757787
Gerar04577 <jemagerard@gmail.com>
2026-09-25 19:55:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kat-a-mar-v19";
+const CACHE = "kat-a-mar-v21";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
